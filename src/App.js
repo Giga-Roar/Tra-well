f022c985e4e630d90b682eb70bbf4ae9d715f5c3
Ajith S P <145632498+Giga-Roar@users.noreply.github.com>
2024-11-06 20:07:21 +0530

    Update App.js v1

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
 import BookingPage from './BookingPage';
 import Team from './Team';
-import './App.css';
+import AdminLogin from './AdminLogin';
+import AdminDashboard from './AdminDashboard';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/booking" element={<BookingPage />} />
-          <Route path='/team' element={<Team />}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
     </Router>
   );
 }
