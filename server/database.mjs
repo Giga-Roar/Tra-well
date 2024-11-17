ddfb22998b6cf555598d573258db32f962c15eea
AathreyaDA <144434793+AathreyaDA@users.noreply.github.com>
2024-11-18 01:13:14 +0530

    Added booker_age column in booking database.mjs

--- server/database.mjs
+++ server/database.mjs
@@ -49,8 +49,8 @@ export const getHotels = async (city_name, min_ratings, max_ratings, city_id) =>
 export const book = async (data) => {
     const bookingCount = (await pool.query("SELECT count(*) as count FROM booking;"))[0][0].count;
     const [returned] = await pool.query(`INSERT INTO booking VALUE
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        `, [bookingCount + 1, data.firstName, data.lastName, data.email, data.hotel_id, (new Date()).toISOString().slice(0, 10), data.checkInDate, data.checkOutDate, 500, data.phoneNumber]);
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `, [bookingCount + 1, data.firstName, data.lastName, data.email, data.hotel_id, (new Date()).toISOString().slice(0, 10), data.checkInDate, data.checkOutDate, 500, data.phoneNumber, data.booker_age]);
     console.log(returned);
 }
 
